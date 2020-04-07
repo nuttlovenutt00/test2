b0a79f973ce733068a2536dd4874f21b30153435
nuttlovenutt00 <60084731+nuttlovenutt00@users.noreply.github.com>
2020-04-07 17:46:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,24 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://d.line-scdn.net/liff/1.0/sdk.js" /></script>
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <button onclick="aa()">กด</button>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
-
-
-   <script>
-    $( document ).ready(function() {
-       liff.init(function (data) {});
-    });
-       function aa(){
-           liff.sendMessages([
-              {
-              type: 'text',
-              text: 'From:' + profile.displayName
-            }
-            ]);
-
-           liff.closeWindows()
-       }
-   
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>My LIFF App</title>
+</head>
+<body>
+  <p id="scanCode"></p>
+  <p>
+    <button id="btnScanCode" onclick="scanCode();">Scan Code</button>
+  </p>
+  <script src="https://static.line-scdn.net/liff/edge/2.1/liff.js"></script>
+  <script>
+    function scanCode() {
+      liff.scanCode().then(result => {
+        const stringifiedResult = JSON.stringify(result);
+        alert(stringifiedResult);
+        document.getElementById("scanCode").textContent = stringifiedResult;
+      });
+    }
+    liff.init({ liffId: "your-liff-id" }, () => {}, err => console.error(err.code, error.message));
   </script>
+</body>
 </html>
